77ceb4b41c60daa121be74950d4365c5985417a2
zaidbarkat174 <zaidbarkat174@gmail.com>
2021-04-14 23:36:47 -0500

    Add test about labels of evaluator

--- tests/test_image_evaluator.cc
+++ tests/test_image_evaluator.cc
@@ -5,13 +5,15 @@
 
 TEST_CASE("Check classify labels") {
     naivebayes::Model model;
-    model.Train(naivebayes::Image::ImagesFromFile("C:\\Users\\zaids\\Cinder\\my-projects\\naive-bayes-ZaidBarkat\\trainingimagesandlabels.txt"));
-    std::vector<naivebayes::Image> images = naivebayes::Image::ImagesFromFile("C:\\Users\\zaids\\Cinder\\my-projects\\naive-bayes-ZaidBarkat\\testimagesandlabelsreal.txt");
+    model.Train(naivebayes::Image::ImagesFromFile(
+            "C:\\Users\\zaids\\Cinder\\my-projects\\naive-bayes-ZaidBarkat\\trainingimagesandlabels.txt"));
+    std::vector<naivebayes::Image> images = naivebayes::Image::ImagesFromFile(
+            "C:\\Users\\zaids\\Cinder\\my-projects\\naive-bayes-ZaidBarkat\\testimagesandlabelsreal.txt");
     naivebayes::ImageEvaluator image_evaluator;
     image_evaluator.Classify(images[0], model);
 
     float increment = 0;
-    for(int i = 0; i < 50; i++) {
+    for (int i = 0; i < 50; i++) {
         image_evaluator.Classify(images[i], model);
 
         if (image_evaluator.GetLabel() == images[i].GetClass()) {
@@ -20,19 +22,19 @@ TEST_CASE("Check classify labels") {
     }
     float accuracy = increment / 50;
 
-    SECTION("Test image file line length") {
+    SECTION("Test accuracy of image evaluator") {
         REQUIRE(0.70 <= Approx(accuracy));
     }
-    SECTION("Test image file line length") {
+    SECTION("Test first image label") {
         REQUIRE(9 == images[0].GetClass());
     }
-    SECTION("Test image file line length") {
+    SECTION("Test second image label") {
         REQUIRE(0 == images[1].GetClass());
     }
-    SECTION("Test image file line length") {
+    SECTION("Test third image label") {
         REQUIRE(2 == images[2].GetClass());
     }
-    SECTION("Test image file line length") {
+    SECTION("Test fourth image label") {
         REQUIRE(5 == images[3].GetClass());
     }
 //    SECTION("Test image file line length") {
